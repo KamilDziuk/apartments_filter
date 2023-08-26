46bc5b47fb15c5d26902b70e9aa3d45968d08748
kamildziuk <kamil.dziuk@o2.pl>
2023-08-26 19:44:31 +0200

    changing data to the database

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 $config = array(
-    'servername' => 'mariadb1011.server15371344.nazwa.pl',
-    'username' => 'server15371344_apartmentfilters',
-    'password' => '4!',
-    'dbname' => 'server142471344_apartmentfilters'
+    'servername' => '',
+    'username' => '',
+    'password' => '',
+    'dbname' => ''
 );
 
 ?>
