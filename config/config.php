720e7eb8d2199b46a4ecdde9617cb29f011107cd
kamildziuk <kamil.dziuk@o2.pl>
2023-08-26 17:31:02 +0200

    add config folder and config.php file

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$config = array(
+    'servername' => 'mariadb1011.server171344.nazwa.pl',
+    'username' => 'server171344_apartmentfilters',
+    'password' => 'JaMalegJacekaDAWTKAIerererererereeEwdawdawd34oKotkAlekaAdew4tgmaIpieska232!',
+    'dbname' => 'server171344_apartmentfilters'
+);
+
+?>
